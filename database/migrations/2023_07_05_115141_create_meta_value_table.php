0ee4139d870ce826fa8f77ee5972866884f2a657
Roma <vedenev2002@gmail.com>
2023-07-05 17:45:23 +0500

    add migrations

--- database/migrations/2023_07_05_115141_create_meta_value_table.php
+++ database/migrations/2023_07_05_115141_create_meta_value_table.php
@@ -11,21 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('meta_value', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('domains_id')->constrained();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->text('value');
             $table->timestamps();
         });
     }
+
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('meta_value');
     }
 };
